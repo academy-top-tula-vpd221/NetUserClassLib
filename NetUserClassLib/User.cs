1116b925c304d528d145540d869cce2234198643
Ефимов Максим <efimov_m@itstep.academy>
2023-03-12 17:51:36 +0300

    Добавьте файлы проекта.

---
+++ NetUserClassLib/User.cs
@@ -0,0 +1,32 @@
+﻿namespace NetUserClassLib
+{
+    public class User
+    {
+        public string Name { set; get; } = "";
+
+        int age = 0;
+
+        public int Age
+        {
+            set
+            {
+                if (value > 0 && value < 100)
+                    age = value;
+                else
+                    age = 0;
+            }
+            get
+            {
+                return age;
+            }
+        }
+
+        public User() { }
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+}
